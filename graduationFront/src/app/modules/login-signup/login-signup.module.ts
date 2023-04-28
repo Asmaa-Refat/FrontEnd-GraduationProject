79e9e368a116c1ca6b89f48c864dabefd1e95038
Asmaa-Refat <asmaarefat071@gmail.com>
2023-04-28 21:18:19 +0300

    renaming

--- graduationFront/src/app/modules/login-signup/login-signup.module.ts
+++ graduationFront/src/app/modules/login-signup/login-signup.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
-import { LoginSignupRoutingModule } from './login|signup-routing.module';
-import { LoginSignupComponent } from './login|signup.component';
+import { LoginSignupRoutingModule } from './login-signup-routing.module';
+import { LoginSignupComponent } from './login-signup.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
