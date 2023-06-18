e1c866b1de7312078ad83ef5b8a1bae0e60e4967
yomnamahmoud100 <yomnaali152@gmail.com>
2023-06-18 13:26:04 +0200

    chart.js

--- graduationFront/src/app/modules/dash-board/dash-board.component.ts
+++ graduationFront/src/app/modules/dash-board/dash-board.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import  Chart  from 'chart.js/auto';
 
 @Component({
   selector: 'app-dash-board',
@@ -10,29 +11,60 @@ export class DashBoardComponent implements OnInit {
 
   serviceName: string = "استخراج بطاقه";
   branchName: string = "branch1";
-  result: any;
+  positiveListCount: number = 0;
+  negativeListCount: number = 0;
+  neutralListCount: number = 0;
 
   constructor(private http: HttpClient) { }
 
-  
+  ngOnInit(): void {
+    this.analyzeSentiment();
+  }
 
   analyzeSentiment(): void {
     const apiURL = 'http://127.0.0.1:8000/serviceReviews/';
-    let encodedText = encodeURIComponent(this.serviceName);
+
     const requestBody = {
       serviceName: this.serviceName,
       branchName: this.branchName
     };
-    
-    this.http.get(apiURL, {params:requestBody}).subscribe({
+
+    this.http.post<any>(apiURL, requestBody).subscribe({
       next: (response) => {
-        console.log(response);
-        this.result = response;
+        this.positiveListCount = response.positiveList.length;
+        this.negativeListCount = response.negativeList.length;
+        this.neutralListCount = response.neutralList.length;
+        this.updateChart();
       },
+      error: (error) => {
+        console.log('Error fetching sentiment analysis data:', error);
+      }
     });
   }
 
-  ngOnInit(): void {
-    this.analyzeSentiment()
+  updateChart(): void {
+    const chartData = {
+      labels: ['Positive', 'Negative', 'Neutral'],
+      datasets: [{
+        label: 'Sentiment Analysis',
+        data: [this.positiveListCount, this.negativeListCount, this.neutralListCount],
+        backgroundColor: ['#00cc99', '#ff6666', '#cccccc']
+      }]
+    };
+
+    const chartOptions = {
+      responsive: true,
+      cutoutPercentage: 80,
+      legend: {
+        display: false
+      }
+    };
+
+    const chart = new Chart('sentimentChart', {
+      type: 'doughnut',
+      data: chartData,
+      options: chartOptions
+    });
   }
+
 }
